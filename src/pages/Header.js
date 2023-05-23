09c4e0450b9eb6d5eef0fe01b4e574c1d244d605
user941211 <common941211@gmail.com>
2023-05-23 21:48:02 +0900

    header logo 클릭 시 firstPage로 Link

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from './img/YUlogo.png';
 import './css/App.css';
-
+import { Link } from 'react-router-dom';
 /*
     로고는 직접만들라고 했으나 창의력의 한계로 인해 어차피 영남대학교 졸업작품이니 영대로고를 png로 다운로드 받아서 배경만 지워서 사용했습니당
     Header에 있는 'YUlogo' 혹은 'Omega Route'를 클릭시에는 firstpage로 이동이 된다.
@@ -12,7 +12,7 @@ function Header(){
       <div className="header">
             <div className="container">
                 <div className="logo">
-                    <img src={logo} alt="logo"></img>
+                <Link to={`/Main`}><img src={logo} alt="logo"></img></Link>
                     <p>Omega Route</p>
                 </div>
             </div>
