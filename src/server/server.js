c17879bacd79634f2650d6b805692df5056a8b42
user941211 <common941211@gmail.com>
2023-05-29 22:01:37 +0900

    mysql db 연결 일부 수정

--- src/server/server.js
+++ src/server/server.js
@@ -1,35 +1,40 @@
-const express = require('express');
+const fs = require('fs');
+
+const express =require('express');
+const bodyParser = require('body-parser');
+const app =express();
+const port = process.env.port || 5000;
+
+app.use(bodyParser.json());//기본적으로 REST API 에서는 데이터 주고받을때 json 데이터 형식으로 주고받음
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+//디비연동 코드
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
 const mysql = require('mysql');
-
-const app = express();
-const port = 3001;
-
-// Create a MySQL connection
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '941211',
-  database: 'tistory'
+    user: "root",
+    password: require("./password.json").mysql.password,
+    host: "127.0.0.1",
+    port: 3306,
+    database: "tistory",
 });
-
-// Connect to the MySQL database
 connection.connect();
 
-// Define a route to fetch data from the database
-app.get('/api/data', (req, res) => {
-  const query = 'SELECT * FROM tistory';
 
-  // Execute the query
-  connection.query(query, (error, results) => {
-    if (error) {
-      res.status(500).json({ error });
-    } else {
-      res.json(results);
-    }
-  });
+
+app.get('/api/customers', (req,res) => {
+    //디비 접근하기
+    connection.query(
+      //쿼리 날리기
+      "select * from CUSTOMER",
+      (err, rows, fields)=>{
+        res.send(rows);
+      }
+       
+    );
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+app.listen(port, ()=> console.log(`listening on port ${port}`));
