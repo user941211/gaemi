1bad70d82fcd00900e7264de58bf1e751abe1d52
user941211 <common941211@gmail.com>
2023-05-30 23:24:04 +0900

    server side 코드 수정!!

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ const app = express();
 const port = 3001;
 require('dotenv').config();
 // sql 연동
-const db = mysql.createConnection({
+const db1 = mysql.createConnection({
   host: process.env.REACT_APP_DB_HOST,
   user: process.env.REACT_APP_DB_USERNAME,
   password: process.env.REACT_APP_DB_PASSWORD,
-  database: process.envREACT_APP_DB_USERNAME,
+  database: process.env.REACT_APP_DB_DATABASE1,
+});
+const db2 = mysql.createConnection({
+  host: process.env.REACT_APP_DB_HOST,
+  user: process.env.REACT_APP_DB_USERNAME,
+  password: process.env.REACT_APP_DB_PASSWORD,
+  database: process.env.REACT_APP_DB_DATABASE2,
 });
 app.use(
   cors({
@@ -19,24 +25,85 @@ app.use(
   })
 );
 // 잘 연동 되었는지 확인
-db.connect((error) => {
+db1.connect((error) => {
   if (error) {
     console.error("Error connecting to MySQL:", error);
     return;
   }
-  console.log("Connected to MySQL database!");
+  console.log("db1 성공");
+});
+db2.connect((error) => {
+  if (error) {
+    console.error("Error connecting to MySQL:", error);
+    return;
+  }
+  console.log("db2 성공");
 });
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-// post 요청 시 값을 객체로 바꿔줌
-app.use(express.urlencoded({ extended: true }));
 
-app.get("/AJ네트웍스", (req, res) => {
+
+app.get("/search", (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   const sql = "SELECT * FROM AJ네트웍스;";
-  db.query(sql, (err, result) => {
+  db1.query(sql, (err, result) => {
     if (err) console.log(err);
     else res.send(result);
   });
+});
+
+// post 요청 시 값을 객체로 바꿔줌
+app.use(express.urlencoded({ extended: true }));
+app.post('/search', (req, res) => {
+  const inputValue = req.body.inputValue;
+
+  // Search for the value in the `hi` and `jk` columns of the `def` table in the `abc` database
+  connection.query(
+    `SELECT * FROM ${process.env.REACT_APP_DB_DATABASE2}.daily_buy_list WHERE code = ? OR code_name = ?`,
+    [inputValue, inputValue],
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+
+      if (results.length === 0) {
+        // If the value doesn't exist, respond with "don't find"
+        return res.json({ message: "don't find" });
+      }
+
+      const jkValue = results[0].jk;
+
+      // Search for a table with the same value and name in the `xyz` database
+      connection.query(
+        `SELECT * FROM information_schema.tables WHERE table_schema = ${process.env.REACT_APP_DB_DATABASE1} AND table_name = ?`,
+        [jkValue],
+        (error, results) => {
+          if (error) {
+            console.error(error);
+            return res.status(500).json({ error: 'Internal Server Error' });
+          }
+
+          if (results.length === 0) {
+            // If the table doesn't exist, respond with "table don't find"
+            return res.json({ message: "table don't find" });
+          }
+
+          const tableName = results[0].table_name;
+
+          // Perform the SELECT * FROM query on the found table
+          connection.query(`SELECT * FROM xyz.${tableName}`, (error, results) => {
+            if (error) {
+              console.error(error);
+              return res.status(500).json({ error: 'Internal Server Error' });
+            }
+
+            // Respond with the result of the SELECT * FROM query
+            res.json(results);
+          });
+        }
+      );
+    }
+  );
 });
--- src/compenents/searchmodal.js
+++ src/compenents/searchmodal.js
@@ -1,12 +1,35 @@
-import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import "../pages/css/App.css";
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import '../pages/css/App.css';
 
 function SearchModal() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/search', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ inputValue }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); //data 출력!
+      } else {
+        console.error('Error:', response.status);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div id="SearchModal">
       <Button variant="primary" className="searchButton" onClick={handleShow}>
@@ -17,10 +40,16 @@ function SearchModal() {
           <Modal.Title>검색어를 입력해주세요</Modal.Title>
         </Modal.Header>
         <Modal.Body id="ModalBody">
-          <input type="search" className="searchSpace" placeholder="검색" />
+          <input
+            type="search"
+            className="searchSpace"
+            placeholder="search"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
         </Modal.Body>
         <Modal.Footer id="ModalFooter">
-          <Button className="firstButton" onClick={handleClose}>
+          <Button className="firstButton" onClick={handleSearch}>
             Search
           </Button>
           <Button
