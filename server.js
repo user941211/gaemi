6efa876380363583f47f4a714f679a8bd8bcd443
user941211 <common941211@gmail.com>
2023-06-01 12:48:51 +0900

    aws db 서버 통신 성공

--- server.js
+++ server.js
@@ -22,26 +22,13 @@ const db2 = mysql.createConnection({
   password: process.env.REACT_APP_DB_PASSWORD,
   database: process.env.REACT_APP_DB_DATABASE2,
 });
-/*app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-    optionsSuccessStatus: 200,
-  })
-);*/
 // 잘 연동 되었는지 확인
 db1.connect((error) => {
-  if (error) {
-    console.error("Error connecting to MySQL:", error);
-    return;
-  }
+  if (error) { console.error("Error connecting to MySQL1:", error); return; }
   console.log("db1 성공");
 });
 db2.connect((error) => {
-  if (error) {
-    console.error("Error connecting to MySQL:", error);
-    return;
-  }
+  if (error) { console.error("Error connecting to MySQL2:", error); return; }
   console.log("db2 성공");
 });
 app.listen(port, () => {
@@ -55,57 +42,47 @@ app.get("/search", (req, res) => {
     else res.send(result);
   });
 });
-var inputValue;
+
+let inputValue;
 // post 요청 시 값을 객체로 바꿔줌
 app.use(express.urlencoded({ extended: true }));
 app.post('/search', (req, res) => {
   inputValue = req.body.name;
-  res.header("Access-Control-Allow-Origin", "*");
-  console.log(inputValue);
-  // Search for the value in the `hi` and `jk` columns of the `def` table in the `abc` database
+  console.log('Received search term:', inputValue);
+  res.header('Access-Control-Allow-Origin', '*');
   db2.query(
     `SELECT * FROM ${process.env.REACT_APP_DB_DATABASE2}.stock_item_all WHERE code = ? OR code_name = ?`,
     [inputValue, inputValue],
-    (error, results) => {
+    (error, db1results) => {
       if (error) {
         console.error(error);
-        return res.status(500).json({ error: 'Internal Server Error1' });
+        return res.status(500).json({ error: 'Internal Server Error' });
       }
-
-      if (results.length === 0) {
-        // If the value doesn't exist, respond with "don't find"
+      if (db1results.length === 0) {
         return res.json({ message: "못 찾겠다.." });
       }
-
-      const jkValue = results[0].jk;
-
-      // db1
+      console.log('db1 결과 : ',db1results);
+      const jkValue = db1results[0].code_name.replace(/'/g, '');
+      console.log(jkValue);
       db1.query(
-        `SELECT * FROM information_schema.tables WHERE table_schema = ${process.env.REACT_APP_DB_DATABASE1} AND table_name = ?`,
-        [jkValue],
+        `SELECT * FROM ${jkValue}`,
         (error, results) => {
           if (error) {
             console.error(error);
-            return res.status(500).json({ error: 'Internal Server Error2' });
+            return res.status(500).json({ error: 'Internal Server Error' });
           }
-
           if (results.length === 0) {
-            // If the table doesn't exist, respond with "table don't find"
             return res.json({ message: "table don't find" });
           }
-
-          const tableName = results[0].table_name;
-
-          // Perform the SELECT * FROM query on the found table
-          db2.query(`SELECT * FROM ${process.env.REACT_APP_DB_DATABASE2}.${tableName}`, (error, results) => {
+          console.log(results);
+          const tableName = results[0].code_name;
+          /*db2.query(`SELECT * FROM ${tableName}`, (error, results) => {
             if (error) {
               console.error(error);
-              return res.status(500).json({ error: 'Internal Server Error3' });
+              return res.status(500).json({ error: 'Internal Server Error' });
             }
-
-            // Respond with the result of the SELECT * FROM query
             res.json(results);
-          });
+          });*/
         }
       );
     }
--- src/compenents/searchmodal.js
+++ src/compenents/searchmodal.js
@@ -3,70 +3,67 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import '../pages/css/App.css';
 import axios from 'axios';
+
 function SearchModal() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  //위는 ㅁㅁㄱ 작성
   const [inputValue, setInputValue] = useState('');
   const [searchResult, setSearchResult] = useState(null);
 
-  
   const handleSearch = async (e) => {
     e.preventDefault();
 
-    setInputValue(e.value);
     try {
-      const response = await axios.post('http://localhost:3001/search', {term : inputValue});
+      const response = await axios.post('http://localhost:3001/search', { name: inputValue });
       const data = response.data;
-      setSearchResult(data); //검색 결과 부모한테 ㄱㄱ
-      console.log(response.data)
+      setSearchResult(data);
+      console.log(response.data);
     } catch (error) {
       console.error('Error:', error);
     }
   };
+
   const handleOnKeyPress = e => {
     if (e.key === 'Enter') {
-      handleSearch(e); // Enter 입력이 되면 클릭 이벤트 실행
+      handleSearch(e);
     }
   };
+
   return (
     <form onSubmit={handleSearch} onKeyDown={handleOnKeyPress}>
       <div id="SearchMrodal">
-          <Button variant="outline-primary" className="searchButton" onClick={handleShow}>
-            찾기
-          </Button>
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>검색어를 입력해주세요</Modal.Title>
-            </Modal.Header>
-            <Modal.Body id="ModalBody">
-              <input
-                type="search"
-                className="searchSpace"
-                placeholder="여섯자리 숫자코드 혹은 이름으로 입력하세요"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-                onKeyDown={handleOnKeyPress}
-              />
-            </Modal.Body>
-            <Modal.Footer id="ModalFooter">
-              <Button className="firstButton" onClick={handleSearch} type='submit'>
-                찾기
-              </Button>
-              <Button
-                variant="outline-secondary"
-                className="secondButton"
-                onClick={handleClose}
-              >
-                Close
-              </Button>
-            </Modal.Footer>
-          </Modal>
+        <Button variant="outline-primary" className="searchButton" onClick={handleShow}>
+          찾기
+        </Button>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>검색어를 입력해주세요</Modal.Title>
+          </Modal.Header>
+          <Modal.Body id="ModalBody">
+            <input
+              type="search"
+              className="searchSpace"
+              placeholder="여섯자리 숫자코드 혹은 이름으로 입력하세요"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              onKeyDown={handleOnKeyPress}
+            />
+          </Modal.Body>
+          <Modal.Footer id="ModalFooter">
+            <Button className="firstButton" type="submit">
+              찾기
+            </Button>
+            <Button variant="outline-secondary" className="secondButton" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     </form>
-    
   );
 }
 
 export default SearchModal;
+
+
