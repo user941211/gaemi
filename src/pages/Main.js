c2a23eb9cdfa77bb4eb50acd1782c99965b80ed3
MinManKey <headwig9898@naver.com>
2023-06-06 05:53:37 +0900

    현재가,매수적정가,매도적정가가 생기고 밑에 progress 파트가 사라졌답니다 여러분~~

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -31,6 +31,11 @@ function Main() {
       <div className="main">
         <div className="main_container">
           <div className="Left">
+            <div>
+              <div className="presentPrice"><p>현재가 : </p><p>7마넌</p></div>
+              <div className="buyPrice"><p>매수적정가 : </p><p>7000~8000</p></div>
+              <div className="sellPrice"><p>매도적정가 : </p><p>7000~8000</p></div>
+            </div>
             <div className="Graph">
               <Chart chartData={chartData}/>
             </div>
