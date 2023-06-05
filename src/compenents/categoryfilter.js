f6105b9502b2bc7f602b0c8fe438420d3be68902
chris <cgd67@naver.com>
2023-06-05 23:03:40 +0900

    종목필터

---
+++ src/compenents/categoryfilter.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import "../pages/css/App.css";
+
+
+function Categoryfilter() {
+  return (
+    <div className="right_move">
+      <Button variant="outline-primary">필터생성</Button>{' '}
+    </div>
+  );
+}
+
+export default Categoryfilter;
