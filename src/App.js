c93f82b8fcce11aba7a43c267c3e62ee894705a1
user941211 <common941211@gmail.com>
2023-10-12 00:53:02 +0900

    routes 관리

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./pages/css/App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Firstpage from "./pages/firstpage";
 import Main from "./pages/Main";
+import SearchWindow from "./compenents/searchwindow";
 /*
   일단 제일처음에 firstpage부터 실행되고 Link to 로 Firstpage애서 Main으로만 변경이 되는 구조이다.
   그리고 Header에 있는 'YUlogo' 혹은 'Omega Route'를 클릭시에는 firstpage로 이동이 된다.
@@ -13,6 +14,7 @@ function App() {
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path={`/`} element={<Firstpage />} />
+        <Route path={`/searchwindow`} element={<SearchWindow />} />
         <Route path={`/main`} element={<Main />} />
       </Routes>
     </Router>
