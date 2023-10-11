cedb4f0d92a92480d1bb25099a6a9241815d5654
user941211 <common941211@gmail.com>
2023-10-12 00:55:06 +0900

    2가지 page에서 오는 props 병렬 처리

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,29 +6,44 @@ import Tabs from "react-bootstrap/Tabs";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Header";
 import "./css/App.css";
+import { useLocation } from "react-router-dom";
 import BalGul from "../compenents/balgul";
 import Price from "../compenents/price";
 import Test from "../compenents/test";
 import FinancialInfo from "../compenents/financialinfo";
 import Recommendation from "../compenents/recommendation";
 import Categoryfilter from "../compenents/categoryfilter";
-//import SearchModal from '../compenents/searchmodal';
+import Searchwindow from '../compenents/searchwindow';
 /*
     종목분석과 발굴분석은 알파스퀘어를 최대한 모방할건데
     한 페이지로 한꺼번에 볼 수 있도록만 변경한다.
 */
 
 function Main() {
-  const [chartData, setChartData] = useState([]);
-  const [finance, setfinance] = useState([]);
-  const [recommend, setrecommend] = useState([]);
-  const [rim, setrim] = useState([]);
+  // const [chartData, setChartData] = useState([]);
+  // const [finance, setfinance] = useState([]);
+  // const [recommend, setrecommend] = useState([]);
+  // const [rim, setrim] = useState([]);
+  // const [userId, setUserId] = useState(
+  //   location.state?.userId
+  // );
+  
+  const location = useLocation();
+  const [chartData, setChartData] = useState(location.state?.results||[]);
+  const [finance, setfinance] = useState(location.state?.finance||[]);
+  const [recommend, setrecommend] = useState(location.state?.recommend||[]);
+  const [rim, setrim] = useState(location.state?.rim||[]);
+
+  //console.log(state);
+
+
   const handleDataUpdate = (results, finance, recommend, rim) => {
     setChartData(results);
     setfinance(finance);
     setrecommend(recommend);
     setrim(rim);
   };
+
   console.log(recommend);
   return (
     <div>
