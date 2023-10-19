03450813d8f0da5ddf947a590f8228626a1e1379
user941211 <common941211@gmail.com>
2023-10-19 16:50:39 +0900

    프로필 헤더 적용

--- src/compenents/profile.js
+++ src/compenents/profile.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "../pages/css/Profile.css";
+import Header from "../pages/Header.js";
 import profileMMK from "../pages/img/profile_mmk.jpg";
 
 function Profile() {
   return (
-    <div id="profile_container">
-      <div className="profile">
-        <img src={profileMMK} alt="profile"></img>
-        <p>민만기</p>
+    <div>
+      <Header link="/profile"/>
+      <div id="profile_container">
+        <div className="profile">
+          <img src={profileMMK} alt="profile"></img>
+          <p>민만기</p>
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
 export default Profile;
