88ae0da617d9d7542c750e4ba2f3dc635114ef0b
MinManKey <headwig9898@naver.com>
2023-10-23 10:44:12 +0900

    프로필이미지 크기조정 완료

--- src/compenents/profileContent.js
+++ src/compenents/profileContent.js
@@ -2,24 +2,24 @@ import React from "react";
 import "../pages/css/ProfileContent.css";
 
 function ProfileContent({ name, part, content, 순서 }) {
-  const 사진=[
+  const 사진 = [
     //mmk, bjs, scw, sjh, kjh
     require("../pages/img/profile_mmk.jpg"),
     require("../pages/img/profile_bjs.jpg"),
     require("../pages/img/profile_scw.jpg"),
     require("../pages/img/profile_sjh.jpg"),
-    require("../pages/img/profile_kjh.jpg")
- ]
+    require("../pages/img/profile_kjh.jpg"),
+  ];
   return (
     <div>
       <div id="profile_content_container">
         <div className="profile">
           <img src={사진[순서]} alt="profile"></img>
-        </div>
-        <div className="profileText">
-          <h2>{name}</h2>
-          <h4>{part}</h4>
-          <p>{content}</p>
+          <div className="profileText">
+            <h2>{name}</h2>
+            <h4>{part}</h4>
+            <p>{content}</p>
+          </div>
         </div>
       </div>
     </div>
