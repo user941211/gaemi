2152115b4f191cca065bd82f49046449d89e7c7d
user941211 <common941211@gmail.com>
2023-11-06 16:55:18 +0900

    charts 외관만 수정(기능은 아직 연동이 안됨)

--- src/compenents/chart.js
+++ src/compenents/chart.js
@@ -2,52 +2,115 @@
 // import ApexCharts from "react-apexcharts";
 
 // function Chart({ chartData }) {
-//   console.log("chartdata" + chartData)
-//   const [series, setSeries] = useState([
+//   const minLow = Math.min(
+//     ...chartData.map((item) => Math.min(item.low, item.high, item.open, item.close))
+//   );
+//   const maxHigh = Math.max(
+//     ...chartData.map((item) => Math.max(item.low, item.high, item.open, item.close))
+//   );
+//   const chartDataWithDateObjects = chartData.map((item) => ({
+//     ...item,
+//     date: new Date(item.date), // item.date를 Date 객체로 변환
+//   }));
+//   const minDate = Math.min(...chartDataWithDateObjects.map((item) => item.date));
+//   const maxDate = Math.max(...chartDataWithDateObjects.map((item) => item.date));
+//   const [candlestickSeries, setCandlestickSeries] = useState([
 //     {
-//       name: " ",
+//       name: "Candlestick",
 //       data: [],
 //     },
 //   ]);
 
-//   const [options, setOptions] = useState({
+//   const [candlestickOptions, setCandlestickOptions] = useState({
 //     chart: {
-//       zoom: {
-//         enabled: false,
+//       type: "candlestick",
+//       height: 300,
+//     },
+//     xaxis: {
+//       type: "category",
+//       categories: [],
+//       labels: {
+//         show: true,
+//         rotate: -45,
 //       },
-//       events: {
-//         dataPointMouseEnter: function (event, chartContext, config) {
-//           const { dataPointIndex, seriesIndex } = config;
-//           const { categories } = chartContext.config.xaxis;
-//           const category = categories[dataPointIndex];
-//           const value = series[seriesIndex].data[dataPointIndex];
 
-//           console.log(`Category: ${category}`);
-//           console.log(`Value: ${value}`);
+//     },
+//     yaxis: [
+//       {
+//         tooltip: {
+//           enabled: true,
 //         },
+//         labels: {
+//           style: {
+//             colors: "#3C90EB",
+//           },
+//         },
+//         min: minLow * 0.98,
+//         max: maxHigh * 1.02,
 //       },
+//     ],
+//   });
+
+//   const [columnOptions, setColumnOptions] = useState({
+//     chart: {
+//       type: "bar",
+//       height: 160,
 //     },
 //     dataLabels: {
 //       enabled: false,
 //     },
-//     stroke: {
-//       curve: "smooth",
-//     },
-//     title: {
-//       text: " ",
-//       align: "left",
-//     },
-//     grid: {
-//       row: {
-//         colors: ["black", "transparent"],
-//         opacity: 0.5,
+//     plotOptions: {
+//       bar: {
+//         columnWidth: "80%",
+//         colors: {
+//           ranges: [
+//             {
+//               from: -1000,
+//               to: 0,
+//               color: "#F15B46",
+//             },
+//             {
+//               from: 1,
+//               to: 10000,
+//               color: "#FEB019",
+//             },
+//           ],
+//         },
 //       },
 //     },
-//     xaxis: {
-//       categories: [],
+//     stroke: {
+//       width: 0,
 //     },
-
+//     xaxis: {
+//       type: "category",
+//       categories: [],
+//       labels: {
+//         show: true,
+//         rotate: -45,
+//       },
+//     },
+//     yaxis: {
+//       labels: {
+//         show: false,
+//       },
+//       tooltip: {
+//         enabled: true,
+//       },
+//       labels: {
+//         formatter: function (value) {
+//           // 값에 1000으로 나눈 몫을 계산하고 "k"를 추가
+//           return (value / 1000).toFixed(1) + "k";
+//         },
+//       },
+//     },
 //   });
+
+//   const [columnSeries, setColumnSeries] = useState([
+//     {
+//       name: "volume",
+//       data: [],
+//     },
+//   ]);
 
 //   useEffect(() => {
 //     updateChartData();
@@ -56,90 +119,97 @@
 //   const updateChartData = () => {
 //     if (chartData && chartData.length > 0) {
 //       const categories = chartData.map((item) => item.date);
-//       const shortenedCategories = categories.map((date) => date.slice(0, 6));
-
-//       const uniqueCategories = Array.from(new Set(categories));
-//       const uniqueMonths = uniqueCategories.map((category) => category.slice(0, 6));
-//       const uniqueCategoriesMerged = [...new Set(uniqueMonths)];
-
-      
-//       //위 const변수는 categories map에서 받은 정보들을 월별로 끊기 위해서 만든거임.
-//       const slicedCategories = uniqueCategoriesMerged.slice(0, 10);
-//       const end = chartData.map((item) => item.close);
-//       const codeName = chartData[0].code_name;
-//       /*const start = chartData.map((item) => item.open);
-//       const low = chartData.map((item) => item.low);
-//       const high = chartData.map((item) => item.high);
-//       const volume = chartData.map((item) => item.volume);*/
-//       const name = chartData[0].code_name;
-//       setSeries([
-//         //{ name: '시가', data: start },
-//         //{ name: '고가', data: high },
-//         //{ name: '저가', data: low },
-//         { name: "종가", data: end },
-//         //{ name: '거래량', data: volume },
+//       const candlestickData = chartData.map((item) => ({
+//         x: item.date,
+//         y: [item.open, item.high, item.low, item.close],
+//       }));
+//       // const volumeData = chartData.map((item) => item.volume);
+//       const volumeData = chartData.map((item) => ({
+//         x: item.date,
+//         y: item.volume
+//       }));
+//       setCandlestickSeries([
+//         {
+//           name: "Candlestick",
+//           data: candlestickData,
+//         },
 //       ]);
 
-//       setOptions((prevOptions) => ({
+//       setCandlestickOptions((prevOptions) => ({
 //         ...prevOptions,
 //         xaxis: {
-//           categories: shortenedCategories,
-//           tickPlacement: "on",
+//           categories,
 //           labels: {
-//             show: true, // 하나의 x축 레이블만 표시하기 위해 레이블 표시를 비활성화합니다.
+//             show: true,
+//             rotate: 0,
+//           },
+//           zoom: {
+//             enabled: true,
+//             type: 'x',
+//             autoScaleXaxis: true,
+//           },
+//         },
+//       }));
+
+//       setColumnSeries([
+//         {
+//           name: "volume",
+//           data: volumeData,
+//         },
+//       ]);
+
+//       setColumnOptions((prevOptions) => ({
+//         ...prevOptions,
+//         xaxis: {
+//           categories,
+//           labels: {
+//             show: false,
 //             rotate: -45,
 //           },
 //         },
-//         stroke: {
-//           width: 3, // 선의 굵기를 여기서 조절함.
+//         zoom: {
+//           enabled: true, // x-축 확대/축소 활성화
+//           type: 'x', // x-축 확대/축소 설정
+//           autoScaleYaxis: true, // y-축을 자동으로 조정
 //         },
-//         title: {
-//           text: codeName,
-//           align: "left",
-//         },
-
-//         colors: ["#FFFFFF"],
 //       }));
 //     }
 //   };
 
 //   return (
 //     <div>
-//       <ApexCharts
-//         options={options}
-//         series={series}
-//         type="line"
-//         width={"100%"}
-//         height={500}
-//       />
+//       <div>
+//         <ApexCharts options={candlestickOptions} series={candlestickSeries} type="candlestick" width={"100%"} height={300} />
+//       </div>
+//       <div>
+//         <ApexCharts options={columnOptions} series={columnSeries} type="bar" height={160} />
+//       </div>
 //     </div>
 //   );
 // }
 
 // export default Chart;
-
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import ApexCharts from "react-apexcharts";
 
 function Chart({ chartData }) {
-  const [candlestickSeries, setCandlestickSeries] = useState([
-    {
-      name: "Candlestick",
-      data: [],
-    },
-  ]);
+  const candlestickChartRef = useRef(null);
+  const brushChartRef = useRef(null);
 
   const [candlestickOptions, setCandlestickOptions] = useState({
     chart: {
       type: "candlestick",
       height: 300,
+      id: "candlestick-chart",
     },
     xaxis: {
-      type: "category",
-      categories: [],
+      type: "datetime",
       labels: {
         show: true,
         rotate: -45,
+      },
+      selection: {
+        enabled: true, // selection 활성화
       },
     },
     yaxis: [
@@ -149,75 +219,32 @@ function Chart({ chartData }) {
         },
         labels: {
           style: {
-            colors: "#3C90EB", // Candlestick 차트의 색상과 맞추어 조절하세요
-          },
-        },
-      },
-      {
-        opposite: true,
-        tooltip: {
-          enabled: true,
-        },
-        labels: {
-          style: {
-            colors: "#F15B46", // Column 차트의 색상과 맞추어 조절하세요
+            colors: "#3C90EB",
           },
         },
       },
     ],
   });
 
-  const [columnOptions, setColumnOptions] = useState({
+  const [brushOptions, setBrushOptions] = useState({
     chart: {
       type: "bar",
       height: 160,
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    plotOptions: {
-      bar: {
-        columnWidth: "80%",
-        colors: {
-          ranges: [
-            {
-              from: -1000,
-              to: 0,
-              color: "#F15B46",
-            },
-            {
-              from: 1,
-              to: 10000,
-              color: "#FEB019",
-            },
-          ],
-        },
-      },
-    },
-    stroke: {
-      width: 0,
+      id: "brush-chart",
     },
     xaxis: {
-      type: "category",
-      categories: [],
-      labels: {
-        show: true,
-        rotate: -45,
-      },
-    },
-    yaxis: {
+      type: "datetime",
       labels: {
         show: false,
       },
     },
+    dataLabels: {
+      enabled: false,
+    },
   });
 
-  const [columnSeries, setColumnSeries] = useState([
-    {
-      name: "volume",
-      data: [],
-    },
-  ]);
+  const [candlestickData, setCandlestickData] = useState([]); // candlestickData 변수 선언
+  const [volumeData, setVolumeData] = useState([]); // volumeData 변수 선언
 
   useEffect(() => {
     updateChartData();
@@ -225,46 +252,47 @@ function Chart({ chartData }) {
 
   const updateChartData = () => {
     if (chartData && chartData.length > 0) {
-      const categories = chartData.map((item) => item.date);
+      const categories = chartData.map((item) =>
+        new Date(
+          item.date.slice(0, 4),
+          item.date.slice(4, 6) - 1,
+          item.date.slice(6)
+        ).getTime()
+      );
+
       const candlestickData = chartData.map((item) => ({
-        x: item.date,
+        x: new Date(
+          item.date.slice(0, 4),
+          item.date.slice(4, 6) - 1,
+          item.date.slice(6)
+        ).getTime(),
         y: [item.open, item.high, item.low, item.close],
       }));
-      const volumeData = chartData.map((item) => item.volume);
 
-      setCandlestickSeries([
-        {
-          name: "Candlestick",
-          data: candlestickData,
-        },
-      ]);
+      const volumeData = chartData.map((item) => ({
+        x: new Date(
+          item.date.slice(0, 4),
+          item.date.slice(4, 6) - 1,
+          item.date.slice(6)
+        ).getTime(),
+        y: item.volume,
+      }));
+
+      setCandlestickData(candlestickData); // candlestickData 변수에 데이터 할당
+      setVolumeData(volumeData); // volumeData 변수에 데이터 할당
 
       setCandlestickOptions((prevOptions) => ({
         ...prevOptions,
         xaxis: {
           categories,
-          labels: {
-            show: true,
-            rotate: -45,
-          },
         },
       }));
 
-      setColumnSeries([
-        {
-          name: "volume",
-          data: volumeData,
-        },
-      ]);
-
-      setColumnOptions((prevOptions) => ({
+      // Brush 차트의 X-축 데이터 업데이트
+      setBrushOptions((prevOptions) => ({
         ...prevOptions,
         xaxis: {
           categories,
-          labels: {
-            show: false,
-            rotate: -45,
-          },
         },
       }));
     }
@@ -273,10 +301,33 @@ function Chart({ chartData }) {
   return (
     <div>
       <div>
-        <ApexCharts options={candlestickOptions} series={candlestickSeries} type="candlestick" width={"100%"} height={300} />
+        <ApexCharts
+          ref={candlestickChartRef}
+          options={candlestickOptions}
+          series={[
+            {
+              name: "Candlestick",
+              data: candlestickData, // candlestickData 변수 사용
+            },
+          ]}
+          type="candlestick"
+          width={"100%"}
+          height={300}
+        />
       </div>
       <div>
-        <ApexCharts options={columnOptions} series={columnSeries} type="bar" height={160} />
+        <ApexCharts
+          ref={brushChartRef}
+          options={brushOptions}
+          series={[
+            {
+              name: "volume",
+              data: volumeData, // volumeData 변수 사용
+            },
+          ]}
+          type="bar"
+          height={160}
+        />
       </div>
     </div>
   );
