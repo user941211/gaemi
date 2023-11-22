b5120d08ddee88f24e8b00c98ed79a218e1978c7
MinManKey <headwig9898@naver.com>
2023-11-22 17:05:48 +0900

    header login.js 연결

---
+++ src/compenents/signup.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from "react-redux"; import { loginUser } from "../reducer/userSlice.js";
+import axios from "axios";
+
+function Signup () {
+return (
+<>
+<form>
+    <h1>회원가입</h1>
+    
+</form>
+</>
+)}
+export default Signup;
